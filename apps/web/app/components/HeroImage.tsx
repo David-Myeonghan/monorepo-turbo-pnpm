103c4c506056b94694bbfeb74d1c47d900c7d628
davidRyu <david.ryu@imagoworks.ai>
2024-09-08 05:54:33 +0900

    apps/web: HeroImage 분리

---
+++ apps/web/app/components/HeroImage.tsx
@@ -0,0 +1,30 @@
+import styles from "./HeroImage.module.css";
+import classNames from "classnames/bind";
+import { Typography } from "@repo/design-system/components";
+
+const cx = classNames.bind(styles);
+
+export default function HeroImage() {
+  return (
+    <div className={cx("hero-image")}>
+      <video
+        style={{ width: "100%" }}
+        poster="https://common-fe.toss.im/resources/adaptive?light=https%3A%2F%2Fcore-cdn-fe.toss.im%2Fvideo%2Fframe%2F%3Fsource%3Dhttps%3A%2F%2Fstatic.toss.im%2F3d%2Ftossface-intro.mp4%26t%3D00%3A00%3A00.000&amp;dark=https%3A%2F%2Fcore-cdn-fe.toss.im%2Fvideo%2Fframe%2F%3Fsource%3Dhttps%3A%2F%2Fstatic.toss.im%2F3d%2Ftossface-intro.mp4%26t%3D00%3A00%3A00.000&amp;lowlight=https%3A%2F%2Fcore-cdn-fe.toss.im%2Fvideo%2Fframe%2F%3Fsource%3Dhttps%3A%2F%2Fstatic.toss.im%2F3d%2Ftossface-intro.mp4%26t%3D00%3A00%3A00.000&amp;lowdark=https%3A%2F%2Fcore-cdn-fe.toss.im%2Fvideo%2Fframe%2F%3Fsource%3Dhttps%3A%2F%2Fstatic.toss.im%2F3d%2Ftossface-intro.mp4%26t%3D00%3A00%3A00.000"
+        autoPlay
+        loop
+        muted
+        playsInline
+      >
+        <source
+          src="https://static.toss.im/3d/tossface-intro.mp4"
+          type="video/mp4"
+        />
+      </video>
+
+      <div className={cx("hero-text")}>
+        <Typography size={"t3"}>함께 만들어가는,</Typography>
+        <Typography size={"t4"}>토스페이스.</Typography>
+      </div>
+    </div>
+  );
+}
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,6 +3,7 @@ import styles from "./page.module.css";
 import classNames from "classnames/bind";
 import { Typography } from "@repo/design-system/components";
 import Header from "./components/Header";
+import HeroImage from "./components/HeroImage";
 
 const cx = classNames.bind(styles);
 
@@ -10,26 +11,7 @@ export default function Home() {
   return (
     <div className={cx("container")}>
       <Header />
-      <div className={cx("hero-image")}>
-        <video
-          style={{ width: "100%" }}
-          poster="https://common-fe.toss.im/resources/adaptive?light=https%3A%2F%2Fcore-cdn-fe.toss.im%2Fvideo%2Fframe%2F%3Fsource%3Dhttps%3A%2F%2Fstatic.toss.im%2F3d%2Ftossface-intro.mp4%26t%3D00%3A00%3A00.000&amp;dark=https%3A%2F%2Fcore-cdn-fe.toss.im%2Fvideo%2Fframe%2F%3Fsource%3Dhttps%3A%2F%2Fstatic.toss.im%2F3d%2Ftossface-intro.mp4%26t%3D00%3A00%3A00.000&amp;lowlight=https%3A%2F%2Fcore-cdn-fe.toss.im%2Fvideo%2Fframe%2F%3Fsource%3Dhttps%3A%2F%2Fstatic.toss.im%2F3d%2Ftossface-intro.mp4%26t%3D00%3A00%3A00.000&amp;lowdark=https%3A%2F%2Fcore-cdn-fe.toss.im%2Fvideo%2Fframe%2F%3Fsource%3Dhttps%3A%2F%2Fstatic.toss.im%2F3d%2Ftossface-intro.mp4%26t%3D00%3A00%3A00.000"
-          autoPlay
-          loop
-          muted
-          playsInline
-        >
-          <source
-            src="https://static.toss.im/3d/tossface-intro.mp4"
-            type="video/mp4"
-          />
-        </video>
-
-        <div className={cx("hero-text")}>
-          <Typography size={"t3"}>함께 만들어가는,</Typography>
-          <Typography size={"t4"}>토스페이스.</Typography>
-        </div>
-      </div>
+      <HeroImage />
     </div>
   );
 }
