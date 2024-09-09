907570c89c249e6fab7853add5d962a255668e31
davidRyu <david.ryu@imagoworks.ai>
2024-09-10 01:33:36 +0900

    AccordionList 분리

---
+++ apps/web/app/components/AccordionList.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useState } from "react";
+import classNames from "classnames/bind";
+import styles from "./AccordionList.module.css";
+import humanizeString from "humanize-string";
+
+const cx = classNames.bind(styles);
+
+type SinglePost = {
+  id: number;
+  title: string;
+  body: string;
+  tags: Array<string>;
+};
+
+interface Props {
+  postList: Array<SinglePost>;
+}
+
+export default function AccordionList({ postList }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+  const accordionActive = (index: number) =>
+    selectedIndex === index ? "open" : "close";
+
+  const handleAccordionClick = (index: number) => {
+    if (selectedIndex === null) {
+      setSelectedIndex(index);
+    } else {
+      setSelectedIndex(null);
+    }
+  };
+
+  return postList?.map((item, index) => (
+    <div
+      key={item.id}
+      className={cx("accordion")}
+      onClick={(e) => handleAccordionClick(index)}
+    >
+      <div className={cx("box")}>
+        <span className={cx("category")}>
+          {humanizeString(item.tags[0] as string)}
+        </span>
+        <span className={cx("title")}>{item.title}</span>
+      </div>
+      <div className={cx(accordionActive(index))}>
+        <span className={cx("description")}>{item.body}</span>
+      </div>
+    </div>
+  ));
+}
--- apps/web/app/components/Questions.tsx
+++ apps/web/app/components/Questions.tsx
@@ -1,24 +1,15 @@
-"use client";
-
 import styles from "./Questions.module.css";
 import classNames from "classnames/bind";
-import { Typography } from "@repo/design-system/components";
-import { useState } from "react";
+import { Loading, Typography } from "@repo/design-system/components";
+import AccordionList from "./AccordionList";
+import { Suspense } from "react";
 
 const cx = classNames.bind(styles);
 
-export default function Questions() {
-  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+export default async function Questions() {
+  const data = await fetch("https://dummyjson.com/posts?limit=5");
+  const response = await data.json();
 
-  const accordionActive = selectedIndex === 1 ? "open" : "close";
-
-  const handleAccordionClick = (index: number) => {
-    if (selectedIndex === null) {
-      setSelectedIndex(index);
-    } else {
-      setSelectedIndex(null);
-    }
-  };
   return (
     <div className={cx("container")}>
       <div className={cx("question")}>
@@ -26,25 +17,9 @@ export default function Questions() {
       </div>
 
       {/*  section */}
-      <div>
-        <div
-          className={cx("accordion")}
-          onClick={(e) => handleAccordionClick(1)}
-        >
-          <div className={cx("box")}>
-            <span className={cx("category")}>저작권</span>
-            <span className={cx("title")}>
-              모두에게 무료라면 저작권이 없나요?
-            </span>
-          </div>
-          <div className={cx(accordionActive)}>
-            <span className={cx("description")}>
-              아니요, 토스페이스의 저작권은 토스 팀에게 있어요. 정확한 사용
-              조건은 토스페이스 저작권 안내를 참고해주세요.
-            </span>
-          </div>
-        </div>
-      </div>
+      <Suspense fallback={<Loading size="medium" />}>
+        <AccordionList postList={response.posts} />
+      </Suspense>
     </div>
   );
 }
